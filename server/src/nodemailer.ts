a4efa5d71f3cbc1dc9bfeccd9da731e21af05dd5
Admin <admin@Admins-MacBook-Air.local>
2026-04-08 14:32:34 +0530

    gmail

--- server/src/nodemailer.ts
+++ server/src/nodemailer.ts
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  host: 'gmail',
+  service: 'gmail',
   // port: 587,
   // secure: false,
   auth: {
