3f5bc7ccf7f4bcb7221056b94a60338d93202ee9
Admin <admin@Admins-MacBook-Air.local>
2026-04-08 14:41:02 +0530

    fixes

--- server/src/nodemailer.ts
+++ server/src/nodemailer.ts
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   // port: 587,
   // secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
   debug: true,
   logger: true
