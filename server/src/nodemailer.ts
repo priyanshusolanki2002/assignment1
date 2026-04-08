88659856428c2af154e58ed8f68eea8a4556b35a
Admin <admin@Admins-MacBook-Air.local>
2026-04-08 14:46:32 +0530

    fixed

--- server/src/nodemailer.ts
+++ server/src/nodemailer.ts
@@ -2,13 +2,14 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  // port: 587,
-  // secure: false,
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4,  // <--- THE SILVER BULLET: Forces IPv4 only
   debug: true,
   logger: true
 });
