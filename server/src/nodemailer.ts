ad40f98c1c556b00cc7cee355c206c07c038b26c
Admin <admin@Admins-MacBook-Air.local>
2026-04-08 14:27:56 +0530

    use gmail as host

--- server/src/nodemailer.ts
+++ server/src/nodemailer.ts
@@ -2,9 +2,9 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  host: 'gmail',
+  // port: 587,
+  // secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
